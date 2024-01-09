8c4690f067f6fa31e4ac5c8e83dc98e9a281617d
Hari Shri Dharmaraj <64031449+harishridharmaraj@users.noreply.github.com>
2024-01-10 00:28:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import UserModel from "./modals/usermodel.js";
 import nodemailer from "nodemailer";
 import randomstring from "randomstring";
